249b98241567dbded208797f1620e339e4ff45e3
vladdumitriu01 <vladdumitriuadrian@gmail.com>
2019-02-20 15:04:20 +0000

    Android NewsApp

---
+++ News Read App/app/src/main/java/com/example/movie/newsapi/NewsDetail.kt
@@ -0,0 +1,42 @@
+package com.example.movie.newsapi
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_news_detail.*
+
+class NewsDetail : AppCompatActivity() {
+
+    lateinit var alertDialog:AlertDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+        alertDialog = SpotsDialog(this)
+        alertDialog.show()
+
+
+
+        //web view
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                alertDialog.dismiss()
+            }
+
+
+        }
+
+        if(intent != null)
+            if(!intent.getStringExtra("webURL").isEmpty())
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                webView.loadUrl(intent.getStringExtra("webURL"))
+    }
+}
